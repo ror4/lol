0ec38f67c36eb3295b2010f5d92ef8d64cc13c61
Aurélien <auron2504@hotmail.fr>
2016-10-28 15:13:35 +0200

    changements sur tableau afficher

--- app/controllers/ChampionsController.java
+++ app/controllers/ChampionsController.java
@@ -4,7 +4,10 @@ import models.Champion;
 import play.mvc.Controller;
 import services.ChampionService;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Formation on 25/10/2016.
@@ -23,6 +26,23 @@ public class ChampionsController extends Controller {
 
     public static void afficher() {
         List<Champion> listeChampion = ChampionService.getAllChampions();
-        renderTemplate("/Lignes/afficher.html",listeChampion);
+        List<Integer> tailleListes = getMaxValueLigne(listeChampion);
+        renderTemplate("/Lignes/afficher.html",listeChampion,tailleListes);
+    }
+
+    public static List<Integer> getMaxValueLigne(List<Champion> listeChampion){
+        List<Integer> longueurLigne = new ArrayList();
+        List<Integer> resultat = new ArrayList();
+        longueurLigne.add(listeChampion.stream().filter(champion -> champion.ligne.equals("Top")).collect(Collectors.toList()).size());
+        longueurLigne.add(listeChampion.stream().filter(champion -> champion.ligne.equals("Jungle")).collect(Collectors.toList()).size());
+        longueurLigne.add(listeChampion.stream().filter(champion -> champion.ligne.equals("Mid")).collect(Collectors.toList()).size());
+        longueurLigne.add(listeChampion.stream().filter(champion -> champion.ligne.equals("Adc")).collect(Collectors.toList()).size());
+        longueurLigne.add(listeChampion.stream().filter(champion -> champion.ligne.equals("Support")).collect(Collectors.toList()).size());
+        for(int i = 0; i <= 4; i++)
+        {
+            resultat.add(longueurLigne.get(i));
+        }
+        resultat.add(Collections.max(longueurLigne));
+        return resultat;
     }
 }
